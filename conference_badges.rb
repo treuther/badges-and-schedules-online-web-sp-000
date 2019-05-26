0651889e39daa3b9c8b4b6996958c536c34ac01e
Tracy Reuther <tracyreuther@gmail.com>
2019-05-26 11:43:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,14 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each do |list|
-    "Hello, my name is #{list}"
+  created_badges = []
+  message = "Hello, my name is #{i}"
+  attendees.each {|i| created_badges << message}
   end
 end
+
+# def square_array(array)
+#   new_array = []
+#   array.each { |i| new_array << i ** 2 }
+#     new_array
+#   end
