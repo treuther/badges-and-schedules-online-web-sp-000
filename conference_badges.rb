3a22512653ec19378b44ff7d9c6d49579b8d54b7
Tracy Reuther <tracyreuther@gmail.com>
2019-05-26 12:27:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,12 +14,8 @@ end
 #   attendees.each_with_index {|list, index| "Hello, #{list}! You'll be assigned to room #{index}!"}
 # end
 
-require "pry"
-
 def assign_rooms(attendees)
   attendees.each_with_index {|list, index| puts "#{list}:#{index}"}
 end
 
-assign_rooms(list)
 
-binding.pry
