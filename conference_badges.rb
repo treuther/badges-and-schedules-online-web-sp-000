e9fe26f0f812dbe74afb4524db29c865f5532010
Tracy Reuther <tracyreuther@gmail.com>
2019-05-26 13:03:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,6 +18,6 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees)
-  assign_rooms(attendees)
+  puts "#{batch_badge_creater(attendees)}"
+  puts "#{assign_rooms(attendees)}"
 end
