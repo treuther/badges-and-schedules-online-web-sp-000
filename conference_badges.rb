bf95621544a9ec1edbfc3e54bee206989188895b
Tracy Reuther <tracyreuther@gmail.com>
2019-05-26 12:40:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,5 +15,5 @@ end
 # end
 
 def assign_rooms(attendees)
-  attendees.collect_with_index {|list, index| puts "#{list}:#{index}"}
+  attendees.map_with_index {|list, index| puts "#{list}:#{index}"}
 end
