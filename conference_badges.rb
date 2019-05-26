093bbbaaf9908e73d1bd5866cc3d2f21686c58e0
Tracy Reuther <tracyreuther@gmail.com>
2019-05-26 11:50:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,15 +10,3 @@ def batch_badge_creator(attendees)
   end
 end
 
-# def batch_badge_creator(attendees)
-#   created_badges = []
-#   message = "Hello, my name is #{i}"
-#   attendees.each { |i| message << i}
-#   message
-# end
-
-# def square_array(array)
-#   new_array = []
-#   array.each { |i| new_array << i ** 2 }
-#     new_array
-#   end
